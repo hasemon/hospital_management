537071358574908ab6baacc7c402cbda3ebf9902
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-12-06 19:27:51 +0600

    contact page style updated

--- js/index.js
+++ js/index.js
@@ -12,3 +12,7 @@ function toContactus() {
 document.getElementById('sendButton').addEventListener('click', () => {
     alert('Thanks for contacting with us.')
 })
+
+document.getElementById('btnConfirmAppointment').addEventListener('click', () => {
+    alert('Your appointment is confirmed. Thanks for staying with us.')
+})
