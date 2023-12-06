40f76aedc40c97e449f4e7f8e1149634f52f3107
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-12-06 18:47:09 +0600

    added more Javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+
+
 function toAppointment() {
     window.location.href = 'appointment.html';
 }
+
+function toContactus() {
+    window.location.href = 'contact.html';
+}
+
+
+document.getElementById('sendButton').addEventListener('click', () => {
+    alert('Thanks for contacting with us.')
+})
